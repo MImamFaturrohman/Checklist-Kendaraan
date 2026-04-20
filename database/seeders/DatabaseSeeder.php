a62b043d377e81d53a0fd5921f1e709841e0eb33
MImamFaturrohman <mimamfaturrohman@gmail.com>
2026-04-20 14:41:54 +0700

    Fix Kendaraan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -50,13 +50,13 @@ class DatabaseSeeder extends Seeder
         // =========================
 
         $kendaraans = [
-            ['nomor_kendaraan' => 'B 2784 PZU', 'jenis_kendaraan' => 'MITSUBISHI XPANDER', 'km_awal' => 3252],
-            ['nomor_kendaraan' => 'B 9394 PAO', 'jenis_kendaraan' => 'TOYOTA HILUX PICK UP', 'km_awal' => 2931],
-            ['nomor_kendaraan' => 'B 9396 PAO', 'jenis_kendaraan' => 'TOYOTA HILUX PICK UP', 'km_awal' => 1234],
-            ['nomor_kendaraan' => 'B 9398 PAO', 'jenis_kendaraan' => 'TOYOTA HILUX PICK UP', 'km_awal' => 5678],
-            ['nomor_kendaraan' => 'B 9400 PAO', 'jenis_kendaraan' => 'TOYOTA HILUX PICK UP', 'km_awal' => 9012],
-            ['nomor_kendaraan' => 'B 9402 PAO', 'jenis_kendaraan' => 'TOYOTA HILUX PICK UP', 'km_awal' => 3456],
-            ['nomor_kendaraan' => 'B 9458 PAO', 'jenis_kendaraan' => 'DAIHATSU GRAN MAX PICK UP', 'km_awal' => 7890],
+            ['nomor_kendaraan' => 'B 2784 PZU', 'jenis_kendaraan' => 'MITSUBISHI XPANDER', 'set_km' => 3252],
+            ['nomor_kendaraan' => 'B 9394 PAO', 'jenis_kendaraan' => 'TOYOTA HILUX PICK UP', 'set_km' => 2931],
+            ['nomor_kendaraan' => 'B 9396 PAO', 'jenis_kendaraan' => 'TOYOTA HILUX PICK UP', 'set_km' => 1234],
+            ['nomor_kendaraan' => 'B 9398 PAO', 'jenis_kendaraan' => 'TOYOTA HILUX PICK UP', 'set_km' => 5678],
+            ['nomor_kendaraan' => 'B 9400 PAO', 'jenis_kendaraan' => 'TOYOTA HILUX PICK UP', 'set_km' => 9012],
+            ['nomor_kendaraan' => 'B 9402 PAO', 'jenis_kendaraan' => 'TOYOTA HILUX PICK UP', 'set_km' => 3456],
+            ['nomor_kendaraan' => 'B 9458 PAO', 'jenis_kendaraan' => 'DAIHATSU GRAN MAX PICK UP', 'set_km' => 7890],
         ];
 
         foreach ($kendaraans as $k) {
@@ -64,6 +64,7 @@ class DatabaseSeeder extends Seeder
                 ['nomor_kendaraan' => $k['nomor_kendaraan']], // 🔑 unik
                 [
                     'jenis_kendaraan' => $k['jenis_kendaraan'],
+                    'set_km' => $k['set_km'],
                 ]
             );
         }
