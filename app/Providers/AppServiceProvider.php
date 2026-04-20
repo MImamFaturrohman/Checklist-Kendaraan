918deae19096c228107ce3d21892f77e93310b15
MImamFaturrohman <mimamfaturrohman@gmail.com>
2026-04-20 08:40:42 +0700

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,8 +22,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
+        if (app()->environment('production')) {
             URL::forceScheme('https');
+
+            Log::info('Migrating and seeding database in production environment');
+            Artisan::call('migrate', ['--force' => true]);
+            Artisan::call('db:seed', ['--force' => true]);
+            Log::info('Database migrated and seeded successfully');
         }
     }
 }
