af1db808fb7d92a8aaa87b39c43073cebdb97abe
MImamFaturrohman <mimamfaturrohman@gmail.com>
2026-07-13 15:26:51 +0700

    Changes: Repair build

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
         strictPort: true,
 
         hmr: {
-            host: '192.168.1.142',
+            host: '192.168.1.169',
             protocol: 'ws',
             port: 5173,
         },
@@ -27,4 +27,7 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        cssMinify: false,
+    },
 });
